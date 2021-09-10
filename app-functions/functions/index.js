f821cfba5445599faa8fa4b31c0bcb50b36d04f4
aydenapollo <mcdonaldshane33@gmail.com>
2021-09-09 20:29:00 -0700

    can now create postings

--- app-functions/functions/index.js
+++ app-functions/functions/index.js
@@ -21,4 +21,23 @@ exports.getPosts = functions.https.onRequest((req, res) => {
       return res.json(posts);
     })
     .catch((err) => console.error(err));
-})
+});
+
+exports.createPost = functions.https.onRequest((req, res) => {
+  const newPost = {
+    body: req.body.body, 
+    userTag: req.body.userTag,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  admin
+  .firestore()
+  .collection('posts')
+  .add(newPost)
+  .then((doc) => {
+    res.json({ message: `document ${doc.id} created successfully`})
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'Something went wrong'});
+    console.error(err);
+  })
+});
