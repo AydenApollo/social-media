24b0d0f5df4b2ab796daaf353bb3113a1b90034d
aydenapollo <mcdonaldshane33@gmail.com>
2021-09-11 12:25:46 -0700

    hitting authentication roadblocks

--- app-functions/functions/index.js
+++ app-functions/functions/index.js
@@ -1,17 +1,37 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
+const firebase = require('firebase');
+const auth = require('firebase-auth');
+const defaultAuth = admin.auth(app);
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyDRSDNmpSVTCWhhQE6BtXVEt9e2LAfQ0mw",
+  authDomain: "socialmediaapp-35e21.firebaseapp.com",
+  projectId: "socialmediaapp-35e21",
+  storageBucket: "socialmediaapp-35e21.appspot.com",
+  messagingSenderId: "631540683163",
+  appId: "1:631540683163:web:5195abaced8098f8cb513e",
+  measurementId: "G-6Q64N2Q62C"
+};
+
+firebase.initializeApp(firebaseConfig)
+const app = require('express')();
+
+
 
 app.get('/posts', (req, res) => {
-  admin.firestore().collection('posts').get()
+  admin.firestore().collection('posts').orderBy('createdAt', 'desc').get()
     .then((data) => {
       let posts = [];
       data.forEach((doc) => {
-        posts.push(doc.data())
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userTag: doc.data().userTag,
+          createdAt: doc.data().createdAt
+        })
       });
       return res.json(posts);
     })
@@ -19,14 +39,11 @@ app.get('/posts', (req, res) => {
 })
 
 
-exports.createPost = functions.https.onRequest((req, res) => {
-  if(req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed'})
-  }
+app.post('/posts',(req, res) => {
   const newPost = {
     body: req.body.body, 
     userTag: req.body.userTag,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
   admin
   .firestore()
@@ -42,5 +59,24 @@ exports.createPost = functions.https.onRequest((req, res) => {
 });
 
 
-// https://baseurl.com/api/
+// signup route
+
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    username: req.body.username,
+  }
+  firebase
+  .auth()
+  .createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then((data) => {
+    return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`})
+  })
+  .catch((err) => {
+    console.error(err);
+    return res.status(500).json({ error: err.code})
+  })
+});
 exports.api = functions.https.onRequest(app);
